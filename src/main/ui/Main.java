b4d23c04ea87793b194c047fbff0344382bc4d14
amy liu <aimyul@alumni.ubc.ca>
2020-02-09 22:07:53 -0800

    implemented Story class with ability to create a file and write text to it

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Pair p = new Pair("dog", "./data/tobs.jpg");
-       // p.paint();
-        //System.out.println(p.getWord());
-        GamesApp g = new GamesApp();
+        //Pair p = new Pair("dog", "./data/tobs.jpg");
+        Scanner input = new Scanner(System.in);
+        MainMenu m = new MainMenu(input);
+        m.runApp();
 
     }
 
